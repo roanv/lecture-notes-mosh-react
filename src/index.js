67aea0d1a72743834cd52f9a9ed7890bbd386435
roanvonb <roanvonb@gmail.com>
2022-06-23 10:45:37 +1000

    created react app

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const element = <h1>Hello World</h1>;
+root.render(element);
