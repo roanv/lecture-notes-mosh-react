f645b50de20e57ba2f9ee066d0707892219418ad
roanvonb <roanvonb@gmail.com>
2022-06-23 11:29:15 +1000

    installed bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const element = <h1>Hello World</h1>;
