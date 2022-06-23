0b679b9efc9b6673370208433c77580d51b80588
roanvonb <roanvonb@gmail.com>
2022-06-23 14:54:26 +1000

    setting up vidly project

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() { return <main className="container" ><h1>Hello World</h1></main>
+    }
+}
+ 
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.css';
 import Counter from './components/counter'
+import App from './app'
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // const element = <h1>Hello World</h1>;
-root.render(<Counter/>);
+root.render(<App/>);
