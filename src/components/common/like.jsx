908e2234437997679c2ab772a02b15a6f560b96a
roanvonb <roanvonb@gmail.com>
2022-06-25 11:05:08 +1000

    changed like heart icon from btn class to clickable

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -5,7 +5,11 @@ const Like = (props) => {
   let icon = "fa-heart fa-";
   props.liked ? (icon += "solid") : (icon += "regular");
   return (
-    <FontAwesomeIcon onClick={props.onClick} icon={icon} className="btn" />
+    <FontAwesomeIcon
+      onClick={props.onClick}
+      icon={icon}
+      className="clickable"
+    />
   );
 };
 
