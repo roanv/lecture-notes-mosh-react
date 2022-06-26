d5e390a9ae6a12e37bd89760a6e158c63bbcd9c5
roanvonb <roanvonb@gmail.com>
2022-06-26 10:08:05 +1000

    fixed button in navbar that was using class instead of classname

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,7 +8,7 @@ const NavBar = ({ totalCounters }) => {
         Vidly
       </Link>
       <button
-        class="navbar-toggler"
+        className="navbar-toggler"
         type="button"
         data-toggle="collapse"
         data-target="#navbarSupportedContent"
@@ -16,7 +16,7 @@ const NavBar = ({ totalCounters }) => {
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"></span>
+        <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <div className="navbar-nav">
